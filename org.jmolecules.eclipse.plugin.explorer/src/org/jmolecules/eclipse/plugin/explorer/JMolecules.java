5624ad3f31bbad4a642d223f4f64a6d5c9528c3d
Ingo Griebsch <ingo.griebsch@gmx.de>
2022-01-18 19:15:25 +0100

    feat: add Onion based concept 'DomainModelRing'

--- org.jmolecules.eclipse.plugin.explorer/src/org/jmolecules/eclipse/plugin/explorer/JMolecules.java
+++ org.jmolecules.eclipse.plugin.explorer/src/org/jmolecules/eclipse/plugin/explorer/JMolecules.java
@@ -92,6 +92,7 @@ class JMolecules {
 
         // Onion Architecture based concepts
         concepts.add(new ApplicationServiceRing());
+        concepts.add(new DomainModelRing());
         concepts.add(new DomainRing());
         return concepts;
     }
@@ -776,6 +777,45 @@ class JMolecules {
         @Override
         public boolean test(IJavaElement source) {
             String fcqn = "org.jmolecules.architecture.onion.classical.ApplicationServiceRing";
+            return testPackage(fcqn, source) || testType(fcqn, source);
+        }
+
+        private boolean testPackage(String fcqn, IJavaElement source) {
+            if (!(source instanceof IPackageDeclaration)) {
+                return false;
+            }
+
+            IPackageDeclaration packageDeclaration = (IPackageDeclaration) source;
+            IAnnotation[] annotations = getAnnotations(packageDeclaration);
+            ICompilationUnit compilationUnit = (ICompilationUnit) packageDeclaration.getParent();
+            IImportDeclaration[] imports = getImports(compilationUnit);
+
+            return test(fcqn, imports, annotations);
+        }
+
+        private boolean testType(String fcqn, IJavaElement source) {
+            if (!(source instanceof IType)) {
+                return false;
+            }
+
+            IType type = (IType) source;
+            IAnnotation[] annotations = getAnnotations(type);
+            IImportDeclaration[] imports = getImports(type.getCompilationUnit());
+
+            return test(fcqn, imports, annotations);
+        }
+    }
+
+    static class DomainModelRing implements AnnotationBasedConcept {
+
+        @Override
+        public Category getCategory() {
+            return ONION_ARCHITECTURE;
+        }
+
+        @Override
+        public boolean test(IJavaElement source) {
+            String fcqn = "org.jmolecules.architecture.onion.classical.DomainModelRing";
             return testPackage(fcqn, source) || testType(fcqn, source);
         }
 

08c4e1ed70dd2560131982465e001d081a4ab30c
Ingo Griebsch <ingo.griebsch@gmx.de>
2022-01-18 19:14:40 +0100

    feat: add Onion based concept 'ApplicationServiceRing'

--- org.jmolecules.eclipse.plugin.explorer/src/org/jmolecules/eclipse/plugin/explorer/JMolecules.java
+++ org.jmolecules.eclipse.plugin.explorer/src/org/jmolecules/eclipse/plugin/explorer/JMolecules.java
@@ -89,6 +89,9 @@ class JMolecules {
         concepts.add(new DomainLayer());
         concepts.add(new InfrastructureLayer());
         concepts.add(new InterfaceLayer());
+
+        // Onion Architecture based concepts
+        concepts.add(new ApplicationServiceRing());
         concepts.add(new DomainRing());
         return concepts;
     }
@@ -734,6 +737,45 @@ class JMolecules {
         @Override
         public boolean test(IJavaElement source) {
             String fcqn = "org.jmolecules.architecture.layered.InterfaceLayer";
+            return testPackage(fcqn, source) || testType(fcqn, source);
+        }
+
+        private boolean testPackage(String fcqn, IJavaElement source) {
+            if (!(source instanceof IPackageDeclaration)) {
+                return false;
+            }
+
+            IPackageDeclaration packageDeclaration = (IPackageDeclaration) source;
+            IAnnotation[] annotations = getAnnotations(packageDeclaration);
+            ICompilationUnit compilationUnit = (ICompilationUnit) packageDeclaration.getParent();
+            IImportDeclaration[] imports = getImports(compilationUnit);
+
+            return test(fcqn, imports, annotations);
+        }
+
+        private boolean testType(String fcqn, IJavaElement source) {
+            if (!(source instanceof IType)) {
+                return false;
+            }
+
+            IType type = (IType) source;
+            IAnnotation[] annotations = getAnnotations(type);
+            IImportDeclaration[] imports = getImports(type.getCompilationUnit());
+
+            return test(fcqn, imports, annotations);
+        }
+    }
+
+    static class ApplicationServiceRing implements AnnotationBasedConcept {
+
+        @Override
+        public Category getCategory() {
+            return ONION_ARCHITECTURE;
+        }
+
+        @Override
+        public boolean test(IJavaElement source) {
+            String fcqn = "org.jmolecules.architecture.onion.classical.ApplicationServiceRing";
             return testPackage(fcqn, source) || testType(fcqn, source);
         }
 

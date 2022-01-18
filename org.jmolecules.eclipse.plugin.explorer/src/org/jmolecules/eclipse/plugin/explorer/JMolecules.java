7699887e2cbc4cb4ccbce7800f4fd60676236257
Ingo Griebsch <ingo.griebsch@gmx.de>
2022-01-18 19:13:40 +0100

    feat: add Layered based concept 'InterfaceLayer'

--- org.jmolecules.eclipse.plugin.explorer/src/org/jmolecules/eclipse/plugin/explorer/JMolecules.java
+++ org.jmolecules.eclipse.plugin.explorer/src/org/jmolecules/eclipse/plugin/explorer/JMolecules.java
@@ -88,6 +88,7 @@ class JMolecules {
         concepts.add(new ApplicationLayer());
         concepts.add(new DomainLayer());
         concepts.add(new InfrastructureLayer());
+        concepts.add(new InterfaceLayer());
         concepts.add(new DomainRing());
         return concepts;
     }
@@ -694,6 +695,45 @@ class JMolecules {
         @Override
         public boolean test(IJavaElement source) {
             String fcqn = "org.jmolecules.architecture.layered.InfrastructureLayer";
+            return testPackage(fcqn, source) || testType(fcqn, source);
+        }
+
+        private boolean testPackage(String fcqn, IJavaElement source) {
+            if (!(source instanceof IPackageDeclaration)) {
+                return false;
+            }
+
+            IPackageDeclaration packageDeclaration = (IPackageDeclaration) source;
+            IAnnotation[] annotations = getAnnotations(packageDeclaration);
+            ICompilationUnit compilationUnit = (ICompilationUnit) packageDeclaration.getParent();
+            IImportDeclaration[] imports = getImports(compilationUnit);
+
+            return test(fcqn, imports, annotations);
+        }
+
+        private boolean testType(String fcqn, IJavaElement source) {
+            if (!(source instanceof IType)) {
+                return false;
+            }
+
+            IType type = (IType) source;
+            IAnnotation[] annotations = getAnnotations(type);
+            IImportDeclaration[] imports = getImports(type.getCompilationUnit());
+
+            return test(fcqn, imports, annotations);
+        }
+    }
+
+    static class InterfaceLayer implements AnnotationBasedConcept {
+
+        @Override
+        public Category getCategory() {
+            return LAYERED_ARCHITECTURE;
+        }
+
+        @Override
+        public boolean test(IJavaElement source) {
+            String fcqn = "org.jmolecules.architecture.layered.InterfaceLayer";
             return testPackage(fcqn, source) || testType(fcqn, source);
         }
 
